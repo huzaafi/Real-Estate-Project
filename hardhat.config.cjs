daf85c7a40a1143badc736639549a57329c41471
huzaafi <huzaifasaleem098@gmail.com>
2023-11-13 22:45:25 +0500

    inital commit

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -4,9 +4,4 @@ require("@nomiclabs/hardhat-ethers");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
-  networks: {
-    hardhat: {
-      chainId: 1337
-    },
-  }
 };
